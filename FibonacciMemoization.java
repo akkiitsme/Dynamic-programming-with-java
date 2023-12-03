e1cb03df4bf4ab7c504cb6a345274c1d8ffb16ee
Akshay singh <61555659+akkiitsme@users.noreply.github.com>
2023-12-03 17:39:35 +0530

    memoization

---
+++ FibonacciMemoization.java
@@ -0,0 +1,26 @@
+package com.dynamic;
+
+import java.util.Scanner;
+
+public class FibonacciMemoization {
+	
+	public static int fibonacci(int n,int[] qb) {
+		if(n==0||n==1)
+			return n;		
+		if(qb[n]==n) {
+			return n;
+		}
+		int fibno = fibonacci(n-1,qb)+fibonacci(n-2,qb);	
+		qb[n]=fibno;
+		return fibno;
+	}
+	
+	
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		int num = sc.nextInt();
+		System.out.println("Fibonacci number is: "+fibonacci(num,new int[num+1]));		
+	}
+
+}
